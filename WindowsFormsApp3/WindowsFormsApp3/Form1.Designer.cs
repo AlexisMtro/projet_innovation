7767eff237dd6ca7e5997bbc6662cf7be201e4e2
Alexis Motro <alexismotro@gmail.com>
2019-01-28 13:43:59 +0100

    v1

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonStopRecord = new System.Windows.Forms.Button();
+            this.ButtonRecord = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonStopRecord
+            // 
+            this.ButtonStopRecord.Location = new System.Drawing.Point(747, 152);
+            this.ButtonStopRecord.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButtonStopRecord.Name = "ButtonStopRecord";
+            this.ButtonStopRecord.Size = new System.Drawing.Size(339, 148);
+            this.ButtonStopRecord.TabIndex = 0;
+            this.ButtonStopRecord.Text = "Cut / Save";
+            this.ButtonStopRecord.UseVisualStyleBackColor = true;
+            this.ButtonStopRecord.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // ButtonRecord
+            // 
+            this.ButtonRecord.Location = new System.Drawing.Point(111, 152);
+            this.ButtonRecord.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ButtonRecord.Name = "ButtonRecord";
+            this.ButtonRecord.Size = new System.Drawing.Size(266, 132);
+            this.ButtonRecord.TabIndex = 1;
+            this.ButtonRecord.Text = "Record";
+            this.ButtonRecord.UseVisualStyleBackColor = true;
+            this.ButtonRecord.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(49, 292);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 231);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ButtonRecord);
+            this.Controls.Add(this.ButtonStopRecord);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonStopRecord;
+        private System.Windows.Forms.Button ButtonRecord;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.IO;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine RecEngin = new SpeechRecognitionEngine();
+        bool CurrentFunction = false; 
+        string path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string command, StringBuilder retstring, int Returnlenth, IntPtr callback);
+        Choices commands = new Choices();
+        Grammar grammar;
+        GrammarBuilder gBuilder;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            mciSendString("open new Type waveaudio alias recsound", null, 0, IntPtr.Zero);
+            ButtonStopRecord.Click += new EventHandler(this.Button1_Click);
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            /*
+            mciSendString("save recsound C:\\Users\\Hadenos_EZ\\Desktop\\Test_Audio\\Value.wav", null, 0, IntPtr.Zero);
+            mciSendString("close recsound ", null, 0, IntPtr.Zero);
+            */
+            try
+            {
+                
+                RecEngin.LoadGrammarAsync(grammar);
+                
+                RecEngin.RecognizeAsyncStop();
+            }
+            catch
+            {
+                MessageBox.Show("Vous n'avez pas de microphone connecté. Vous devez avoir un microphone pour utiliser l'application correctement.", "Erreur");
+
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if(CurrentFunction == false)
+            {
+
+                pictureBox1.Image = Image.FromFile("C:\\Users\\celia\\Desktop\\testg.gif");
+                commands.Add(new string[] { "test", "print my name" });
+                gBuilder = new GrammarBuilder();
+                gBuilder.Append(commands);
+                grammar = new Grammar(gBuilder);
+                RecEngin.LoadGrammarAsync(grammar);
+                RecEngin.SetInputToDefaultAudioDevice();
+                RecEngin.RecognizeAsync(RecognizeMode.Multiple);
+                RecEngin.SpeechRecognized += RecognizeWord;
+                CurrentFunction = true;
+            }
+            else
+            {
+                RecEngin.RecognizeAsyncStop();
+                CurrentFunction = false;
+                pictureBox1.Image = null; 
+            }
+
+
+        }
+        public void WriteFile(string fn)
+        {
+            string filename = System.IO.Path.Combine(path, fn);
+            if (System.IO.File.Exists(filename))
+            {
+                using (var sw = new StreamWriter(filename, true))
+                {
+
+                    sw.WriteLine("On a reussi à écrire dans le fichier.");
+
+                }
+            }
+        }
+        void RecognizeWord(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "test":
+                    WriteFile("testInnovation.txt");
+                    break;
+                case "print my name":
+                    break;
+            }
+        }
+
+    }
+}
